7d7157cd282280ae1eeb3d47066908edaa0d79e1
ljh4770 <ljh4770@naver.com>
2024-08-05 10:47:47 +0900

    lib/db.ts: ssl config

--- lib/db.ts
+++ lib/db.ts
@@ -5,10 +5,12 @@ const connectionString = process.env.POSTGRES_URL;
 const pool = new Pool({
   connectionString,
   // Use SSL in production environment
-  ssl: process.env.NODE_ENV === 'production'
-    ? { rejectUnauthorized: false }
-    : false, // No SSL for local development
+  ssl: { rejectUnauthorized: false }
 });
+
+// ssl: process.env.NODE_ENV === 'production'
+//     ? { rejectUnauthorized: false }
+//     : false, // No SSL for local development
 
 // export async function query(text, params) {
 //   const res = await pool.query(text, params);
