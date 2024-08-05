b1caa4583a45bbea346a75946d107d10d86a4c00
ljh4770 <ljh4770@naver.com>
2024-08-05 13:18:16 +0900

    lib/db.ts : add env var info

--- lib/db.ts
+++ lib/db.ts
@@ -5,8 +5,13 @@ const connectionString = process.env.POSTGRES_URL;
 const pool = new Pool({
   connectionString,
   // Use SSL in production environment
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE
 });
+
 
 // ssl: process.env.NODE_ENV === 'production'
 //     ? { rejectUnauthorized: false }
